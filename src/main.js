0d433813789499d5c79e6248edd34b6d131c48d6
JohnnatanV <joh.villaneda@outlook.com>
2025-06-12 01:38:54 -0500

    Create a functional add to cart btn, add the shopping cart render, missing adjustments

--- src/main.js
+++ src/main.js
@@ -3,71 +3,94 @@ const container = document.querySelector(".container");
 const cart = document.querySelector("#cart");
 const cartContainer = document.querySelector(".cart");
 
-const cartContent = []
+const shoppingCart = [];
 
 const products = [
-  { id: 1, name: "Soda Saborizada", description: "Soda", price: `$${19.99}`, img: images[0] },
-  { id: 2, name: "Blue Alaska", description: "Bebida Alcoholica", price: `$${29.99}`, img: images[1] },
-  { id: 3, name: "Mick Jagger", description: "Bebida Alcoholica", price: `$${39.99}`, img: images[2] },
-]
+  {
+    id: 1,
+    name: "Soda Saborizada",
+    description: "Soda",
+    price: `$${19.99}`,
+    img: images[0],
+  },
+  {
+    id: 2,
+    name: "Blue Alaska",
+    description: "Bebida Alcoholica",
+    price: `$${29.99}`,
+    img: images[1],
+  },
+  {
+    id: 3,
+    name: "Mick Jagger",
+    description: "Bebida Alcoholica",
+    price: `$${39.99}`,
+    img: images[2],
+  },
+];
+
+const renderCart = () => {
+  const cartBox = document.getElementById("cart-box")
+  cartBox.innerHTML = ""
+
+  shoppingCart.forEach(item => {
+    const cartItem = document.createElement("li")
+    cartItem.classList = "cart-item"
+
+    cartItem.innerHTML =
+      `
+      <div class="item">
+        <h4>${item.product.name}</h4>
+        <span>${item.product.price}</span>
+        <span>${item.quantity}</span>
+      </div>
+      <img src="${item.product.img}" alt="${item.product.name}" />
+      <button class="remove-item>R</button>
+    `
+    cartBox.appendChild(cartItem)
+  })
+}
+
+const addToCart = (itemId) => {
+  const product = products.find(p => p.id === itemId)
+  const itemCart = shoppingCart.find(item => item.product.id === itemId)
+
+  if (itemCart) {
+    itemCart.quantity += 1
+  } else {
+    shoppingCart.push({
+      product,
+      quantity: 1
+    })
+  }
+  renderCart()
+}
 
 products.forEach((product) => {
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.setAttribute("data-id", product.id);
+  const card = document.createElement("div")
+  card.classList = "card"
+  card.setAttribute("data-id", `${product.id}`)
 
-  const img = document.createElement("img");
-  img.src = product.img;
-  img.alt = product.name;
-  img.id = product.id;
+  const addBtn = document.createElement("button")
+  addBtn.innerText = "Add to Cart"
+  addBtn.classList = "btn"
+  addBtn.addEventListener("click", () => addToCart(product.id))
 
-  const cardDesc = document.createElement("div");
-  cardDesc.classList.add("card-desc");
+  card.innerHTML = `
+    <img src="${product.img}" alt="${product.name}"
+    <div class="card-desc">
+      <h2>${product.name}</h2>
+      <p>${product.description}</p>
+      <span>${product.price}</span>
+    </div>
+  `
+  card.appendChild(addBtn)
+  container.appendChild(card)
+});
 
-  const title = document.createElement("h2");
-  title.textContent = product.name;
-
-  const desc = document.createElement("p");
-  desc.textContent = product.description;
-
-  const price = document.createElement("span");
-  price.textContent = product.price;
-
-  const button = document.createElement("button");
-  button.classList.add("btn");
-  button.textContent = "Agregar al carrito";
-
-  card.appendChild(img);
-  card.appendChild(cardDesc);
-  cardDesc.appendChild(title);
-  cardDesc.appendChild(desc)
-  cardDesc.appendChild(price);
-  card.appendChild(button);
-
-  container.appendChild(card);
-})
-
-const buttons = document.querySelectorAll(".btn");
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const elem = parseInt(e.target.parentElement.dataset.id);
-    const cartItem = products.map(item => {
-      if (item.id === elem) {
-        console.log({ ...item })
-        return { ...item }
-      }
-      return null;
-    })
-    console.log(cartItem)
-  })
-})
 
 
 cart.addEventListener("click", () => {
   cartContainer.classList.toggle("hide");
-})
+});
 
-cartContent.forEach((item) => {
-  console.log(item)
-})
