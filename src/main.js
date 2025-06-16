b29500195c5009f5df0eedf9c25314e4db84b52d
JohnnatanV <joh.villaneda@outlook.com>
2025-06-16 18:38:21 -0500

    Finished practice ecommercce proyect... remenber is just practice.

--- src/main.js
+++ src/main.js
@@ -10,45 +10,87 @@ const products = [
     id: 1,
     name: "Soda Saborizada",
     description: "Soda",
-    price: `$${19.99}`,
+    price: 19.99,
     img: images[0],
   },
   {
     id: 2,
     name: "Blue Alaska",
     description: "Bebida Alcoholica",
-    price: `$${29.99}`,
+    price: 29.99,
     img: images[1],
   },
   {
     id: 3,
     name: "Mick Jagger",
     description: "Bebida Alcoholica",
-    price: `$${39.99}`,
+    price: 39.99,
     img: images[2],
   },
 ];
 
 const renderCart = () => {
   const cartBox = document.getElementById("cart-box");
+  const totalPrice = document.getElementById("total-price");
+  const tooltip = document.getElementById("tooltip");
+
   cartBox.innerHTML = "";
+
+  let total = 0;
+  let tooltipQuantity = 0;
 
   shoppingCart.forEach((item) => {
     const cartItem = document.createElement("li");
     cartItem.classList = "cart-item";
 
+    const subtotal = item.product.price * item.quantity;
+    total += subtotal;
+    tooltipQuantity += item.quantity;
+
     cartItem.innerHTML = `
       <div class="item">
         <h4>${item.product.name}</h4>
-        <span>${item.product.price}</span>
+        <span>$${item.product.price.toFixed(2)}</span>
         <span>${item.quantity}</span>
       </div>
       <img src="${item.product.img}" alt="${item.product.name}" />
-      <button class="remove-item>R</button>
+      <button class="remove-item" data-id="${item.product.id}">R</button>
     `;
     cartBox.appendChild(cartItem);
   });
+
+  // Mostrar la sumatoria del gasto total
+  totalPrice.innerText = `Total: $${total.toFixed(2)}`;
+  tooltip.innerText = tooltipQuantity;
+
+  // Mostrar tooltip si hay products
+  if (shoppingCart.length > 0) {
+    tooltip.classList.remove("hide-tooltip");
+  } else {
+    tooltip.classList.add("hide-tooltip");
+  }
 };
+
+cartContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove-item")) {
+    const itemId = parseInt(e.target.getAttribute("data-id"));
+    const itemIndex = shoppingCart.findIndex(
+      (item) => item.product.id === itemId,
+    );
+
+    if (itemIndex !== -1) {
+      // Restar uno
+      shoppingCart[itemIndex].quantity -= 1;
+
+      // Si llega a 0, eliminar array
+      if (shoppingCart[itemIndex].quantity === 0) {
+        shoppingCart.splice(itemIndex, 1);
+      }
+
+      renderCart(); // Volver a renderizar o pintar el carrito
+    }
+  }
+});
 
 const addToCart = (itemId) => {
   const product = products.find((p) => p.id === itemId);
